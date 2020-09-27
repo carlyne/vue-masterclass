9b6d16d811e2bbc8f36fc5e2f8304dcc5a5d518a
Carly <carlyne.lapierre@gmail.com>
2020-09-27 19:11:03 +0200

    structure

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div v-for="thread in threads" :key="thread['.key']" class="col-large push-top">
+      <h1>{{ thread.title }}</h1>
+
+      <div class="post-list">
+        <div v-for="postId in thread.posts" :key="postId['.key']" class="post">
+          <div class="user-info">
+            <a href="#" class="user-name">{{ users[posts[postId].userId].name }}</a>
+
+            <a href="profile.html#profile-details">
+              <img
+                class="avatar-large"
+                :src="users[posts[postId].userId].avatar"
+                alt=""
+              />
+            </a>
+
+            <p class="desktop-only text-small">107 posts</p>
+          </div>
+
+          <div class="post-content">
+            <div>
+             {{ posts[postId].text }}
+            </div>
+          </div>
+
+          <div class="post-date text-faded">{{ posts[postId].publishedAt }}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import srcData from "../data.json";
+
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      threads: srcData.threads,
+      posts: srcData.posts,
+      users: srcData.users,
+    };
+  },
+};
+</script>
